6bf56af7df9fa0eff7411604edee4a75f1323b76
Jarold Wong <jaroldw@gmail.com>
2017-04-13 11:33:24 -0700

    Format search results

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var app = express();
 var KEY = process.env.SEARCHAPIKEY;
 var CSE = process.env.CSE;
 var API = "https://www.googleapis.com/customsearch/v1?key=" + KEY + "&cx=" + CSE + "&searchType=image&q=";
+var results;
+
 
 app.get('/:queryTerms?', function (req, res) {
     const options = API + req.params.queryTerms;
@@ -17,10 +19,21 @@ app.get('/:queryTerms?', function (req, res) {
         });
 
         response.on('end', function () {
-            res.send(JSON.parse(data));
+            var results = JSON.parse(data).items;
+
+            var formattedResults = results.map(function(obj) {
+              return {
+                url: obj.link,
+                snippet: obj.snippet,
+                thumbnail: obj.image.thumbnailLink,
+                context: obj.image.contextLink
+              };
+            })
+
+            res.send(formattedResults);
         });
     }
-    
+
     https.request(options, callback).end();
 })
 
